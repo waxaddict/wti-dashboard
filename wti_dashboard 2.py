d5cf6ac751e329428cd25fbb8abaacfabdd7e14f
waxaddict <richard@waxaddict.co.uk>
2025-04-01 13:04:25 +0100

    Update wti_dashboard 2.py

--- wti_dashboard 2.py
+++ wti_dashboard 2.py
@@ -14,7 +14,7 @@ st.markdown("Directional Bias Checklist – Version 2.0")
 # Get Live WTI Daily Data
 # -----------------------
 symbol = "CL=F"
-daily_data = yf.download(tickers=symbol, period="7d", interval="1d", progress=False)
+daily_data = yf.download(tickers=symbol, period="70d", interval="1d", progress=False)
 
 if len(daily_data) < 2:
     st.error("Insufficient daily WTI data. Try again later.")
